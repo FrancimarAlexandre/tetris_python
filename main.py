36bae9651c048902b7cc2bd803c4dc956b2ec1f4
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-03-03 20:22:14 -0300

    criando cores

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+# inicializando o pygame
+
+pygame.init()
+
+# CORES
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (128, 0, 128)
+CYAN = (0, 255, 255)
+ORANGE = (255, 165, 0)
+PINK = (255, 20, 147)
+DARKGREEN = (0, 100, 0)
+
